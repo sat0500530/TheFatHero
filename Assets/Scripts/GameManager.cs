65a48046ed0b5d82ed4fcb9f66f4971e84b7d9cc
sat0500530 <sat0500530@gmail.com>
2023-09-17 17:37:12 +0900

    휴식 아티펙트 버그 수정
    휴식 아티펙트 버그 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -463,7 +463,7 @@ public class GameManager : MonoBehaviour
 
         if (knight.Cost >= 1)
         {
-            knight.Status.CurrentHp += knight.Cost * 2;
+            knight.Status.CurrentHp += knight.Cost * 2 + _dataManager.KnightRestRecoveryHpAddValue;
             knight.Status.Hunger += knight.Cost;
             knight.Cost = 0;
             UIManager.Instance.UpdateKnightStatusInfo();
@@ -596,7 +596,7 @@ public class GameManager : MonoBehaviour
                     // 용사가 서 있는 위치 전달 
                     if (knight.Cost > 0)
                     {
-                        _uiManager.ActiveSomeThingBox($"휴식하시겠습니까?\n(체력+{knight.Cost*2}, 배부름 지수+{knight.Cost})", Rest);
+                        _uiManager.ActiveSomeThingBox($"휴식하시겠습니까?\n(체력+{knight.Cost*2 + _dataManager.KnightRestRecoveryHpAddValue}, 배부름 지수+{knight.Cost})", Rest);
                     }
                     else
                     {
